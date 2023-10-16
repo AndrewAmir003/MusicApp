044bf87f6ebcc6cd4d880d24a5cedcc30b349892
“AndrewAmir003” <andrewamir003@gmail.com>
2023-10-16 22:43:14 +0100

    changed return in likedSongs.jsx

--- src/likedSongs.jsx
+++ src/likedSongs.jsx
@@ -12,13 +12,14 @@ export default function SongList() {
 
   const isLiked = (songName) => likedSongs[songName];
 
+
   const songs = [
     { name: 'Galway Girl', releaseDate: '17-03-2017', coverArt: '../images/EdSheeranGG.jpg' },
     { name: 'Shape of you', releaseDate: '06-01-2017', coverArt: '../images/ShapeOfYou.png' },
     { name: 'Shivers', releaseDate: '10-09-2021', coverArt: '../images/shivers.png'}];
 
   return (
-    <div>
+    <>
       <h2>Songs/Albums</h2>
       <ul>
         {songs.map((song) => (
@@ -32,7 +33,7 @@ export default function SongList() {
           </li>
         ))}
       </ul>
-    </div>
+    </>
   );
 };
 
